4a9b6cd020cadd5307ba9458190f14dbf3689bac
HAoRAn-W <haoran2326@gmail.com>
2022-05-21 18:01:31 -0400

    Add StringUtil for String check

---
+++ src/main/java/one/whr/utils/StringUtil.java
@@ -0,0 +1,17 @@
+package one.whr.utils;
+
+public class StringUtil {
+
+    // check is a String is null or zero length or contains only whitespaces
+    public static boolean isBlank(String s) {
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        for(int i = 0; i < s.length(); i++) {
+            if(!Character.isWhitespace(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
