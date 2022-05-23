6c3be8966eab432b54bdb3ffdf2574c53803a175
HAoRAn-W <haoran2326@gmail.com>
2022-05-23 16:16:01 -0400

    add load balancer

---
+++ src/main/java/one/whr/loadbalance/loadbalancer/ConsistentHashLoadBalancer.java
@@ -0,0 +1,91 @@
+package one.whr.loadbalance.loadbalancer;
+
+import lombok.extern.slf4j.Slf4j;
+import one.whr.loadbalance.AbstractLoadBalancer;
+import one.whr.remote.dto.RpcRequest;
+
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+public class ConsistentHashLoadBalancer extends AbstractLoadBalancer {
+    // store selectors for different services
+    private final ConcurrentHashMap<String, ConsistentHashSelector> selectors = new ConcurrentHashMap<>();
+
+    @Override
+    protected String doSelect(List<String> serviceAddresses, RpcRequest rpcRequest) {
+        int indentityHashCode = System.identityHashCode(serviceAddresses);
+        String rpcServiceName = rpcRequest.getRpcServiceName();
+        ConsistentHashSelector selector = selectors.get(rpcServiceName);
+
+        if(selector == null || selector.identityHashCode != indentityHashCode) {
+            selectors.put(rpcServiceName, new ConsistentHashSelector(serviceAddresses, 160, indentityHashCode));
+            selector = selectors.get(rpcServiceName);
+        }
+        return selector.select(rpcServiceName + Arrays.stream(rpcRequest.getParameters()));
+
+    }
+
+    static class ConsistentHashSelector {
+        // store workers for the service
+        private final TreeMap<Long, String> virtualInvokers;
+        private final int identityHashCode;
+
+        ConsistentHashSelector(List<String> invokers, int replicaNumber, int identityHashCode) {
+            this.virtualInvokers = new TreeMap<>();
+            this.identityHashCode = identityHashCode;
+
+            for (String invoker : invokers) {
+                for (int i = 0; i < replicaNumber / 4; i++) {
+                    byte[] digest = md5(invoker + i);
+                    for (int j = 0; j < 4; j++) {
+                        long m = hash(digest, j);
+                        virtualInvokers.put(m, invoker);
+                    }
+                }
+            }
+        }
+        static byte[] md5(String key) {
+            MessageDigest md;
+            try {
+                md = MessageDigest.getInstance("MD5");
+                byte[] bytes = key.getBytes(StandardCharsets.UTF_8);
+                md.update(bytes);
+            } catch (NoSuchAlgorithmException e) {
+                throw new IllegalStateException(e.getMessage(), e);
+            }
+
+            return md.digest();
+        }
+
+        static long hash(byte[] digest, int idx) {
+            return ((long) (digest[3 + idx * 4] & 255) << 24 |
+                    (long) (digest[2 + idx * 4] & 255) << 16 |
+                    (long) (digest[1 + idx * 4] & 255) << 8 |
+                    (long) (digest[idx * 4] & 255)) &
+                    4294967295L;
+        }
+
+        public String select(String rpcServiceKey) {
+            byte[] digest = md5(rpcServiceKey);
+            return selectByKey(hash(digest, 0));
+        }
+
+        public String selectByKey(long hashCode) {
+            Map.Entry<Long, String> entry = virtualInvokers.tailMap(hashCode, true).firstEntry();
+            if(entry == null) {
+                entry = virtualInvokers.firstEntry();
+            }
+            return entry.getValue();
+        }
+    }
+
+
+}
