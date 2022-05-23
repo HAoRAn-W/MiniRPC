022f846d2a24fc3535787b30adfc89056a0f9c5a
HAoRAn-W <haoran2326@gmail.com>
2022-05-23 18:04:28 -0400

    add PropertiesUtils to read properties

---
+++ src/main/java/one/whr/utils/PropertiesUtils.java
@@ -0,0 +1,38 @@
+package one.whr.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+@Slf4j
+public class PropertiesUtils {
+    private PropertiesUtils() {
+    }
+
+    public static Properties readPropertiesFile(String fileName) {
+        URL url = Thread.currentThread().getContextClassLoader().getResource("");
+        String rpcConfigPath = "";
+        if (url != null) {
+            rpcConfigPath = url.getPath() + fileName;
+        }
+        Properties properties = null;
+        try (InputStreamReader inputStreamReader = new InputStreamReader(
+                Files.newInputStream(Paths.get(rpcConfigPath)), StandardCharsets.UTF_8
+        )) {
+            properties = new Properties();
+            properties.load(inputStreamReader);
+        }
+        catch (IOException e) {
+            log.error("IOException occurs when read properties file [{}]", fileName);
+        }
+        return properties;
+    }
+
+}
