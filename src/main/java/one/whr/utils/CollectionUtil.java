06769a60fe044590b80a9ebdd0195c87207bed92
HAoRAn-W <haoran2326@gmail.com>
2022-05-23 16:15:46 -0400

    add CollectionUtil

---
+++ src/main/java/one/whr/utils/CollectionUtil.java
@@ -0,0 +1,10 @@
+package one.whr.utils;
+
+import java.util.Collection;
+
+public class CollectionUtil {
+    public static boolean isEmpty(Collection<?> c) {
+        return c == null || c.isEmpty();
+    }
+
+}
