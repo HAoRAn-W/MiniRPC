76880c5833267531aed2f696c99373ded1dca700
HAoRAn-W <haoran2326@gmail.com>
2022-05-25 00:41:14 -0400

    bug fix

--- example-server/src/main/java/one/whr/service/impl/HelloServiceImpl.java
+++ example-server/src/main/java/one/whr/service/impl/HelloServiceImpl.java
@@ -1,10 +1,12 @@
 package one.whr.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import one.whr.annotation.RpcService;
 import one.whr.service.Hello;
 import one.whr.service.HelloService;
 
 @Slf4j
+@RpcService(group = "test1", version = "version1")
 public class HelloServiceImpl implements HelloService {
     static {
         System.out.println("HelloServiceImpl created");
