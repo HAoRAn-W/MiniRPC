912f0b3205c1d0aef43d9a5388a35d4b13c9736e
HAoRAn-W <haoran2326@gmail.com>
2022-05-25 00:31:19 -0400

    add client

--- example-client/src/main/java/NettyClientMain.java
+++ example-client/src/main/java/NettyClientMain.java
@@ -3,7 +3,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 @RpcScan(basePackage = {"one.whr"})
 public class NettyClientMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(NettyClientMain.class);
+        HelloController helloController = (HelloController) applicationContext.getBean("helloController");
+        helloController.test();
     }
 }
